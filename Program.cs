e61c7a4c20fe55b24ecfca145cd24a2b0feb960b
Mihaylov1VV <vitalii.mihailov1995@gmail.com>
2025-05-10 12:54:34 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Exercise4;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Задание 4. Напишите программу, которая считывает из консоли фразу и сохраняет её.
+        //Зачем считывает вторую фразу и выводит на экран консоли обе этих фраы 
+        
+        string frasa1;
+        Console.WriteLine("Введите первую фразу: ");
+        frasa1 = Console.ReadLine();
+        
+        string frasa2;
+        Console.Write("Введите вторую фразу: ");
+        frasa2 = Console.ReadLine();
+        Console.WriteLine(frasa1 + " " + frasa2);
+        // У меня не получилось сделать так, чтобы введенная первая фраза не выводилась сразу на консоль
+    }
+}
